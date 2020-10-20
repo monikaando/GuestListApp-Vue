99afe9728d637b79ac4d7372905cdc259e964039
Monika Swidzinska <monika@pxlwidgets-heroes.com>
2020-10-20 11:22:52 +0200

    [Add]Project

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 new Vue({
-  el: '#app',
+  el: "#app",
   data: {
     event: {
       eventDate: "August 14th - 16th",
@@ -7,6 +7,17 @@ new Vue({
       eventDescription:
         "It's back! This years summer festival will be in the beautiful countryside featuring our best line up ever!",
     },
-    newNameText: ''
+    newNameText: "",
+    guestName: [],
+    formSubmitClass: ''
+  },
+  methods: {
+    formSubmitted: function () {
+      if (this.newNameText.length > 0) {
+        this.guestName.push(this.newNameText);
+        this.newNameText = "";
+        this.formSubmitClass = "submitted";
+      }
+    },
   },
 });
